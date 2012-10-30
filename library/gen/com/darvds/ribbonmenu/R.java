60da100c4190663a0e9ae51727fc1eb8de0362a3
mdelolmo <manueldelolmo84@gmail.com>
2012-10-30 13:19:30 +0100

    Support for right-side menu. and iconless menu items

--- library/gen/com/darvds/ribbonmenu/R.java
+++ library/gen/com/darvds/ribbonmenu/R.java
@@ -10,31 +10,79 @@ package com.darvds.ribbonmenu;
 public final class R {
     public static final class anim {
         public static int rbm_in_from_left=0x7f030000;
-        public static int rbm_out_to_left=0x7f030001;
+        public static int rbm_in_from_right=0x7f030001;
+        public static int rbm_out_to_left=0x7f030002;
+        public static int rbm_out_to_right=0x7f030003;
     }
     public static final class attr {
+        /** <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>left</code></td><td>0</td><td></td></tr>
+<tr><td><code>right</code></td><td>1</td><td></td></tr>
+</table>
+         */
+        public static int mode=0x7f010000;
     }
     public static final class color {
-        public static int rbm_item_text_color=0x7f040001;
-        public static int rbm_menu_background=0x7f040000;
+        public static int rbm_item_text_color=0x7f050001;
+        public static int rbm_menu_background=0x7f050000;
     }
     public static final class dimen {
-        public static int rbm_item_image_height=0x7f050003;
-        public static int rbm_item_image_width=0x7f050004;
-        public static int rbm_item_padding_leftright=0x7f050002;
-        public static int rbm_item_padding_topbottom=0x7f050001;
-        public static int rbm_item_text_padding_left=0x7f050006;
-        public static int rbm_item_text_size=0x7f050005;
-        public static int rbm_menu_width=0x7f050000;
+        public static int rbm_item_image_height=0x7f060003;
+        public static int rbm_item_image_width=0x7f060004;
+        public static int rbm_item_padding_leftright=0x7f060002;
+        public static int rbm_item_padding_topbottom=0x7f060001;
+        public static int rbm_item_text_padding_left=0x7f060006;
+        public static int rbm_item_text_size=0x7f060005;
+        public static int rbm_menu_width=0x7f060000;
     }
     public static final class id {
-        public static int rbm_item_icon=0x7f060000;
-        public static int rbm_item_text=0x7f060001;
-        public static int rbm_listview=0x7f060002;
-        public static int rbm_outside_view=0x7f060003;
+        public static int left=0x7f040000;
+        public static int rbm_item_icon=0x7f040002;
+        public static int rbm_item_text=0x7f040003;
+        public static int rbm_listview=0x7f040004;
+        public static int rbm_outside_view=0x7f040005;
+        public static int right=0x7f040001;
     }
     public static final class layout {
         public static int rbm_item=0x7f020000;
-        public static int rbm_menu=0x7f020001;
+        public static int rbm_menu_left=0x7f020001;
+        public static int rbm_menu_right=0x7f020002;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a RibbonMenuView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #RibbonMenuView_mode com.darvds.ribbonmenu:mode}</code></td><td></td></tr>
+           </table>
+           @see #RibbonMenuView_mode
+         */
+        public static final int[] RibbonMenuView = {
+            0x7f010000
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.darvds.ribbonmenu.R.attr#mode}
+          attribute's value can be found in the {@link #RibbonMenuView} array.
+
+
+          <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>left</code></td><td>0</td><td></td></tr>
+<tr><td><code>right</code></td><td>1</td><td></td></tr>
+</table>
+          @attr name android:mode
+        */
+        public static final int RibbonMenuView_mode = 0;
+    };
 }
--- library/src/com/darvds/ribbonmenu/RibbonMenuView.java
+++ library/src/com/darvds/ribbonmenu/RibbonMenuView.java
@@ -1,23 +1,22 @@
 package com.darvds.ribbonmenu;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.xmlpull.v1.XmlPullParser;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
 import android.graphics.Bitmap;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -29,141 +28,137 @@ import android.widget.TextView;
 
 public class RibbonMenuView extends LinearLayout {
 
+	private static final int TYPE_LEFT = 0;
+	private static final int TYPE_RIGHT = 1;
 	private ListView rbmListView;
 	private View rbmOutsideView;
-	
+
 	private iRibbonMenuCallback callback;
-	
+
 	private ArrayList<RibbonMenuItem> menuItems;
-	
+
 	private RibbonMenuAdapter adapter;
-	
+	private int type = TYPE_LEFT;
+
 	public RibbonMenuView(Context context) {
 		super(context);
-		
-		
 		load();
 	}
-	
+
 	public RibbonMenuView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-
+		TypedArray a = context.obtainStyledAttributes(attrs,
+				R.styleable.RibbonMenuView);
+		for (int i = 0; i < a.getIndexCount(); i++) {
+			int attr = a.getIndex(i);
+			if (attr == R.styleable.RibbonMenuView_mode) {
+				type = a.getInt(attr, TYPE_LEFT);
+				break;
+			}
+		}
 		load();
 	}
 
+	private void load() {
 
-	
-	
-	private void load(){
-		
-		if(isInEditMode()) return;
-		
-		
-		inflateLayout();		
-		
+		if (isInEditMode())
+			return;
+
+		inflateLayout();
+
 		initUi();
-		
-		
+
 	}
-	
-	
-	private void inflateLayout(){
-		
-		
-		
-		
-		try{
-			LayoutInflater.from(getContext()).inflate(R.layout.rbm_menu, this, true);
-			} catch(Exception e){
-				
-			}	
-		
-		
+
+	private void inflateLayout() {
+		try {
+			if (type == TYPE_RIGHT) {
+				LayoutInflater.from(getContext()).inflate(
+						R.layout.rbm_menu_right, this, true);
+			} else {
+				LayoutInflater.from(getContext()).inflate(R.layout.rbm_menu_left,
+						this, true);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	private void initUi(){
-		
+
+	private void initUi() {
+
 		rbmListView = (ListView) findViewById(R.id.rbm_listview);
 		rbmOutsideView = (View) findViewById(R.id.rbm_outside_view);
-				
+
 		rbmOutsideView.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				hideMenu();
-				
+
 			}
 		});
-		
-		
+
 		rbmListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				
-				if(callback != null){
+
+				if (callback != null) {
 					callback.RibbonMenuItemClick((int) id);
 				}
 				hideMenu();
 			}
-			
+
 		});
-			
-		
+
 	}
-	
-	
-	public void setMenuClickCallback(iRibbonMenuCallback callback){
+
+	public void setMenuClickCallback(iRibbonMenuCallback callback) {
 		this.callback = callback;
 	}
-	
-	public void addHeader(View headerView){
+
+	public void addHeader(View headerView) {
 		rbmListView.addHeaderView(headerView, null, false);
 	}
-	
-	public void setMenuItems(int menu){
-		
+
+	public void setMenuItems(int menu) {
+
 		parseXml(menu);
-		
-		if(menuItems != null && menuItems.size() > 0)
-		{
-			if (adapter != null){
+
+		if (menuItems != null && menuItems.size() > 0) {
+			if (adapter != null) {
 				rbmListView.setAdapter(adapter);
-			}else{
+			} else {
 				rbmListView.setAdapter(new RibbonMenuAdapter());
 			}
-			
+
 		}
-		
-	
+
 	}
-	
-	public void setMenuItems(List<? extends RibbonMenuItem> menu){
-		if (menuItems == null){
+
+	public void setMenuItems(List<? extends RibbonMenuItem> menu) {
+		if (menuItems == null) {
 			menuItems = new ArrayList<RibbonMenuView.RibbonMenuItem>();
 		}
 		menuItems.addAll(menu);
-		
-		if(menuItems != null && menuItems.size() > 0)
-		{
-			if (adapter == null){
+
+		if (menuItems != null && menuItems.size() > 0) {
+			if (adapter == null) {
 				adapter = new RibbonMenuAdapter();
 			}
 			rbmListView.setAdapter(adapter);
 			adapter.notifyDataSetChanged();
-			
+
 		}
-		
-	
+
 	}
-	
-	public void setBackgroundResource(int resource){
+
+	public void setBackgroundResource(int resource) {
 		rbmListView.setBackgroundResource(resource);
-		
+
 	}
-	
-	
+
 	public RibbonMenuAdapter getAdapter() {
 		return adapter;
 	}
@@ -172,176 +167,176 @@ public class RibbonMenuView extends LinearLayout {
 		this.adapter = adapter;
 	}
 
-	public void showMenu(){
-		rbmOutsideView.setVisibility(View.VISIBLE);	
-				
-		rbmListView.setVisibility(View.VISIBLE);	
-		Animation loadAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.rbm_in_from_left);
+	public void showMenu() {
+		rbmOutsideView.setVisibility(View.VISIBLE);
+
+		rbmListView.setVisibility(View.VISIBLE);
+
+		Animation loadAnimation;
+		if (type == TYPE_RIGHT) {
+			loadAnimation = AnimationUtils.loadAnimation(getContext(),
+					R.anim.rbm_in_from_right);
+		} else {
+			loadAnimation = AnimationUtils.loadAnimation(getContext(),
+					R.anim.rbm_in_from_left);
+		}
 		rbmListView.startAnimation(loadAnimation);
 	}
-	
-	
-	public void hideMenu(){
-		
+
+	public void hideMenu() {
 		rbmOutsideView.setVisibility(View.GONE);
-		rbmListView.setVisibility(View.GONE);	
-		
-		rbmListView.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.rbm_out_to_left));
-		
+		rbmListView.setVisibility(View.GONE);
+
+		if (type == TYPE_RIGHT) {
+			rbmListView.startAnimation(AnimationUtils.loadAnimation(
+					getContext(), R.anim.rbm_out_to_right));
+		} else {
+			rbmListView.startAnimation(AnimationUtils.loadAnimation(
+					getContext(), R.anim.rbm_out_to_left));
+		}
+
 	}
-	
-	
-	public void toggleMenu(){
-		if(rbmOutsideView.getVisibility() == View.GONE){
+
+	public void toggleMenu() {
+		if (rbmOutsideView.getVisibility() == View.GONE) {
 			showMenu();
 		} else {
 			hideMenu();
 		}
 	}
-	
-	
-	private void parseXml(int menu){
+
+	private void parseXml(int menu) {
 		menuItems = new ArrayList<RibbonMenuView.RibbonMenuItem>();
-		
-		try{
+
+		try {
 			XmlResourceParser xpp = getResources().getXml(menu);
-			
+
 			xpp.next();
 			int eventType = xpp.getEventType();
-			
-			while(eventType != XmlPullParser.END_DOCUMENT){
-				
-				if(eventType == XmlPullParser.START_TAG){
-					
+
+			while (eventType != XmlPullParser.END_DOCUMENT) {
+
+				if (eventType == XmlPullParser.START_TAG) {
+
 					String elemName = xpp.getName();
-						
-					
-					
-					if(elemName.equals("item")){
-											
-						
-						String textId = xpp.getAttributeValue("http://schemas.android.com/apk/res/android", "title");
-						String iconId = xpp.getAttributeValue("http://schemas.android.com/apk/res/android", "icon");
-						String resId = xpp.getAttributeValue("http://schemas.android.com/apk/res/android", "id");
-						
-						
+
+					if (elemName.equals("item")) {
+
+						String textId = xpp.getAttributeValue(
+								"http://schemas.android.com/apk/res/android",
+								"title");
+						String iconId = xpp.getAttributeValue(
+								"http://schemas.android.com/apk/res/android",
+								"icon");
+						String resId = xpp.getAttributeValue(
+								"http://schemas.android.com/apk/res/android",
+								"id");
+
 						RibbonMenuItem item = new RibbonMenuItem();
 						item.id = Integer.valueOf(resId.replace("@", ""));
 						item.text = resourceIdToString(textId);
-						item.iconRes = Integer.valueOf(iconId.replace("@", ""));
-						
+						if (iconId != null){
+							item.iconRes = Integer.valueOf(iconId.replace("@", ""));
+						}
+
 						menuItems.add(item);
 					}
-					
+
 				}
-				
+
 				eventType = xpp.next();
-				
-				
+
 			}
-			
-			
-		} catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	private String resourceIdToString(String text){
-		
-		if(!text.contains("@")){
+
+	private String resourceIdToString(String text) {
+
+		if (!text.contains("@")) {
 			return text;
 		} else {
-									
+
 			String id = text.replace("@", "");
-									
+
 			return getResources().getString(Integer.valueOf(id));
-			
+
 		}
-		
+
 	}
-	
-	
-	public boolean isMenuVisible(){		
-		return rbmOutsideView.getVisibility() == View.VISIBLE;		
+
+	public boolean isMenuVisible() {
+		return rbmOutsideView.getVisibility() == View.VISIBLE;
 	}
-	
-		
-	
-	
-	@Override 
-	protected void onRestoreInstanceState(Parcelable state)	{
-	    SavedState ss = (SavedState)state;
-	    super.onRestoreInstanceState(ss.getSuperState());
 
-	    if (ss.bShowMenu)
-	        showMenu();
-	    else
-	        hideMenu();
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		SavedState ss = (SavedState) state;
+		super.onRestoreInstanceState(ss.getSuperState());
+
+		if (ss.bShowMenu)
+			showMenu();
+		else
+			hideMenu();
 	}
-	
-	
 
-	@Override 
-	protected Parcelable onSaveInstanceState()	{
-	    Parcelable superState = super.onSaveInstanceState();
-	    SavedState ss = new SavedState(superState);
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		Parcelable superState = super.onSaveInstanceState();
+		SavedState ss = new SavedState(superState);
 
-	    ss.bShowMenu = isMenuVisible();
+		ss.bShowMenu = isMenuVisible();
 
-	    return ss;
+		return ss;
 	}
 
 	static class SavedState extends BaseSavedState {
-	    boolean bShowMenu;
+		boolean bShowMenu;
 
-	    SavedState(Parcelable superState) {
-	        super(superState);
-	    }
+		SavedState(Parcelable superState) {
+			super(superState);
+		}
 
-	    private SavedState(Parcel in) {
-	        super(in);
-	        bShowMenu = (in.readInt() == 1);
-	    }
+		private SavedState(Parcel in) {
+			super(in);
+			bShowMenu = (in.readInt() == 1);
+		}
 
-	    @Override
-	    public void writeToParcel(Parcel out, int flags) {
-	        super.writeToParcel(out, flags);
-	        out.writeInt(bShowMenu ? 1 : 0);
-	    }
+		@Override
+		public void writeToParcel(Parcel out, int flags) {
+			super.writeToParcel(out, flags);
+			out.writeInt(bShowMenu ? 1 : 0);
+		}
 
-	    public static final Parcelable.Creator<SavedState> CREATOR
-	            = new Parcelable.Creator<SavedState>() {
-	        public SavedState createFromParcel(Parcel in) {
-	            return new SavedState(in);
-	        }
+		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+			public SavedState createFromParcel(Parcel in) {
+				return new SavedState(in);
+			}
 
-	        public SavedState[] newArray(int size) {
-	            return new SavedState[size];
-	        }
-	    };
+			public SavedState[] newArray(int size) {
+				return new SavedState[size];
+			}
+		};
 	}
-	
-	
-	
-	public class RibbonMenuItem{
+
+	public class RibbonMenuItem {
 		public int id;
 		public String text;
 		public int iconRes;
 		public Bitmap iconBmp;
 	}
-	
-	
-	
+
 	public class RibbonMenuAdapter extends BaseAdapter {
 
 		private LayoutInflater inflater;
-		
-		public RibbonMenuAdapter(){
+
+		public RibbonMenuAdapter() {
 			inflater = LayoutInflater.from(getContext());
 		}
-		
+
 		@Override
 		public int getCount() {
 			return menuItems.size();
@@ -354,59 +349,55 @@ public class RibbonMenuView extends LinearLayout {
 
 		@Override
 		public Object getItem(int position) {
-			if (menuItems != null){
+			if (menuItems != null) {
 				return menuItems.get(position);
-			}else{
+			} else {
 				return null;
 			}
 		}
 
 		@Override
-		public long getItemId(int position) {			
+		public long getItemId(int position) {
 			return menuItems.get(position).id;
 		}
-		
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			
+
 			ViewHolder holder;
-			
-			if(convertView == null || convertView instanceof TextView){
+
+			if (convertView == null || convertView instanceof TextView) {
 				convertView = inflater.inflate(R.layout.rbm_item, null);
-				
+
 				holder = new ViewHolder();
-				holder.image = (ImageView) convertView.findViewById(R.id.rbm_item_icon);
-				holder.text = (TextView) convertView.findViewById(R.id.rbm_item_text);
-						
+				holder.image = (ImageView) convertView
+						.findViewById(R.id.rbm_item_icon);
+				holder.text = (TextView) convertView
+						.findViewById(R.id.rbm_item_text);
+
 				convertView.setTag(holder);
-			
-			} else {			
+
+			} else {
 				holder = (ViewHolder) convertView.getTag();
 			}
-			if (menuItems.get(position).iconBmp != null){
+			if (menuItems.get(position).iconBmp != null) {
 				holder.image.setImageBitmap(menuItems.get(position).iconBmp);
-			}else{
+			} else {
 				holder.image.setImageResource(menuItems.get(position).iconRes);
 			}
-			holder.text.setText(menuItems.get(position).text);
-			
-			
+			if (menuItems.get(position).text != null) {
+				holder.text.setText(menuItems.get(position).text);
+			}
+
 			return convertView;
 		}
-		
-		
+
 		public class ViewHolder {
 			public TextView text;
 			public ImageView image;
-		
-		}
-			
-		
-		
-		
-	}
-	
 
+		}
+
+	}
 
 }
